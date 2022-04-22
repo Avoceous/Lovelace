c14eb8c1ac93e85995eb4b8b0de489c7bac42ace
Allen Charls Casul <66356528+softDev28@users.noreply.github.com>
2022-04-22 22:28:19 +0800

    Add files via upload

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,7 @@
 //Loading Animation
 $(window).on('load', () => {
-    $('#loaderContainer').fadeOut(800);
-    $('.mainBody').fadeIn(800);
+    $('#loaderContainer').fadeOut(400);
+    $('.mainBody').fadeIn(400);
 });
 
 //Click Counter Function
@@ -43,37 +43,27 @@ const showSearchTransparentEffectWhenSearchIconClicked = document.getElementById
 showSearchTransparentEffectWhenSearchIconClicked.addEventListener('click', () => {
     document.getElementById('searchBarContainer').style.display = 'block';
     $(document).ready(() => {
-        $('#searchTransparentBackground').fadeIn(120);
+        $('#searchTransparentBackground').fadeIn(100);
     });
 });
 
 const hideSearchTransparentEffectWhenBackArrowClicked = document.getElementById('searchBackArrow');
 hideSearchTransparentEffectWhenBackArrowClicked.addEventListener('click', () => {
     $(document).ready(() => {
-        $('#searchTransparentBackground').fadeOut(120);
-        $('#searchBarContainer').fadeOut(120);
+        $('#searchTransparentBackground').fadeOut(100);
+        $('#searchBarContainer').fadeOut(100);
     });
 });
 
 const hideSearchTransparentEffectWhenTapped = document.getElementById('searchTransparentBackground');
 hideSearchTransparentEffectWhenTapped.addEventListener('click', () => {
     $(document).ready(() => {
-        $('#searchTransparentBackground').fadeOut(120);
-        $('#searchBarContainer').fadeOut(120);
+        $('#searchTransparentBackground').fadeOut(100);
+        $('#searchBarContainer').fadeOut(100);
     });
 });
 
 //Redirect Function
-const homeRedirect = document.getElementById('homeIcon');
-homeRedirect.addEventListener('click', () => {
-    window.location.href = './';
-});
-
-const homeTextRedirect = document.getElementById('selected');
-homeTextRedirect.addEventListener('click', () => {
-    window.location.href = './';
-});
-
 const info = document.getElementById('info');
 info.addEventListener('click', () => {
     window.location.href = 'https://www.facebook.com/allencasul';
@@ -84,15 +74,15 @@ const showNightThemeTransparentEffect = document.getElementById('theme');
 showNightThemeTransparentEffect.addEventListener('click', () => {
     document.getElementById('nightThemeContainer').style.display = 'block';
     $(document).ready(() => {
-        $('#nightThemeTransparentBackground').fadeIn(120);
+        $('#nightThemeTransparentBackground').fadeIn(100);
     });
 });
 
 const hideNightThemeModalWhenTapped = document.getElementById('nightThemeTransparentBackground');
 hideNightThemeModalWhenTapped.addEventListener('click', () => {
     $(document).ready(() => {
-        $('#nightThemeTransparentBackground').fadeOut(120);
-        $('#nightThemeContainer').fadeOut(120);
+        $('#nightThemeTransparentBackground').fadeOut(100);
+        $('#nightThemeContainer').fadeOut(100);
     });
 });
 
@@ -117,7 +107,6 @@ function changeStatus() {
 }
 
 //This function will save night theme in local storage
-checkStatus();
 function checkStatus() {
     if (localStorage.getItem('darkMode') === "true") {
         check.checked = true;
@@ -126,4 +115,4 @@ function checkStatus() {
         check.checked = false;
         link.href = '';
     }
-}
+}checkStatus();
